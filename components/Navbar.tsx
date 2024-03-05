3a4a3ff2afb13627caf8bdf16ed0ba48aa61fc36
Daniel Magro <dando385@yahoo.com>
2024-03-04 23:25:44 -0500

    added shadcn Select component/ui for navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,7 +1,18 @@
+// components/Navbar.tsx
+import { Select } from './ui/Select'; // Adjust the import path as necessary
+
 const Navbar = () => {
-  return (
-    <div>Navbar</div>
-  )
-}
+    return (
+        <nav className="bg-green-400 p-4">
+            <div className="flex justify-between items-center">
+                <div className="text-slate-900 font-electrolize">
+                    <h1>DAN MAGRO</h1>
+                    <p>Blockchain Developer</p>
+                </div>
+                <Select />
+            </div>
+        </nav>
+    );
+};
 
 export default Navbar;
