f7db17942ec446623e128663dc341738607392b7
Daniel Magro <dando385@yahoo.com>
2024-05-06 20:21:29 -0400

    updated colors on project cards

--- components/ProjectCard.tsx
+++ components/ProjectCard.tsx
@@ -2,11 +2,11 @@
 
 const ProjectCard = ({ name, languages, description, repoLink }) => {
   return (
-    <div className="flex flex-col items-start justify-between p-6 bg-white rounded-lg border border-gray-200 shadow-md hover:bg-gray-100">
+    <div className="flex flex-col items-start justify-between p-6 bg-white rounded-lg border border-green-200 shadow-md hover:bg-green-100">
       <h3 className="text-lg font-semibold">{name}</h3>
-      <p className="text-sm text-gray-800">Technologies Used: {languages}</p>
-      <p className="text-sm text-gray-600">{description}</p>
-      <a href={repoLink} className="mt-4 text-blue-600 hover:underline">GitHub Repository</a>
+      <p className="text-sm text-greennn-800">Technologies Used: {languages}</p>
+      <p className="text-sm text-green-400">{description}</p>
+      <a href={repoLink} className="mt-4 text-green-400 hover:underline">GitHub Repository</a>
     </div>
   );
 };
